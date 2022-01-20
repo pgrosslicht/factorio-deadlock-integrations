55c4bd5a5e014ecd446c5ab5118b1802f50a5880
Patrick Grosslicht <patrick@grosslicht.com>
2022-01-20 19:08:45 +0100

    add krastorio

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,31 @@
 if mods["DoubleSpeedBelts"] then
 	require("prototypes.doublespeed")
 end
+
+-- -- Inter/Cross compatibility with deadlock beltboxes loaders with IndustrialRevolution
+-- -- And Krastorio renaming
+if mods["pdg-deadlock-beltboxes-loaders"] and not mods["IndustrialRevolution"] then
+  local function krCreateStacks(_stackable_krastorio_items)
+    for i, items in pairs(_stackable_krastorio_items) do
+      for _, item_name in pairs(items) do
+        ---@diagnostic disable-next-line
+        deadlock.destroy_stack(item_name)
+        ---@diagnostic disable-next-line
+        deadlock.add_stack(item_name, nil, "deadlock-stacking-" .. i)
+      end
+    end
+  end
+
+  stackable_krastorio_items = {
+    [1] = {
+      "iron-plate",
+      "copper-plate",
+      "iron-gear-wheel",
+    },
+    [2] = { -- Tier 2
+      "steel-plate",
+    },
+  }
+
+  krCreateStacks(stackable_krastorio_items)
+end
